d571a9f4f8d6e108ad41953828b661e3240188da
Azul De Pol <azul.depol@gmail.com>
2023-03-19 21:42:08 +0000

    clone el repo y agregue el maquetado y todos los enunciados de los ejercicios

---
+++ js/TP3-ejercicio2.js
@@ -0,0 +1,22 @@
+/*
+Dificultad:  🟢🟡
+2-  Crear un script que solicite al usuario mediante un prompt el nombre de ciudades y almacenarlas en un arreglo, cuando el usuario selecciona cancelar se debe mostrar el arreglo generado, luego realizar las siguientes acciones:
+
+Mostrar la longitud del arreglo.
+Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+Añade en última posición la ciudad de París.
+Escribe por pantalla el elemento que ocupa la segunda posición.
+Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
+
+Ejemplo:
+Input:
+[‘Nueva York, Estados Unidos’, ‘Barcelona, España’, ‘Tokio, Japón’, ‘Londres, Reino Unido’, ‘Roma, Italia’, ‘Pekín, China’, ‘Río de Janeiro, Brasil’, ‘Ámsterdam, Países Bajos’, ‘Sídney, Australia’, ‘El Cairo, Egipto’]
+
+Output:
+
+*/ 
+
+let cities = [];
+let city = prompt('Ingrese una ciudad');
+
+document.write ("<h2>Lista de Ciudades</h2>") 
