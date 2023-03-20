6636e747f579b61115080f132bdfed7ad029398c
Azul De Pol <azul.depol@gmail.com>
2023-03-20 21:33:32 +0000

    trabaje el ejercicio 2 hasta pedir  ciudades dentro de un arreglo y mostrarlas al cancelar el arreglo

--- js/TP3-ejercicio2.js
+++ js/TP3-ejercicio2.js
@@ -11,12 +11,39 @@ Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona
 Ejemplo:
 Input:
 [‘Nueva York, Estados Unidos’, ‘Barcelona, España’, ‘Tokio, Japón’, ‘Londres, Reino Unido’, ‘Roma, Italia’, ‘Pekín, China’, ‘Río de Janeiro, Brasil’, ‘Ámsterdam, Países Bajos’, ‘Sídney, Australia’, ‘El Cairo, Egipto’]
+Amsterdam
+Budapest
+Copenhagen
+Dubai
+Edinburgh
+Frankfurt
+Geneva
+Hanoi
+Indianapolis
+Johannesburg
+Paris
+Barcelona
+
 
 Output:
 
 */ 
 
 let cities = [];
-let city = prompt('Ingrese una ciudad');
 
-document.write ("<h2>Lista de Ciudades</h2>") 
+
+document.write ("<h2>Lista de Ciudades</h2>");
+document.write("<ul>");
+
+for (let index = 0; index < 11; index++) {
+   let city = prompt('Ingrese una ciudad');
+   cities.push(city);
+   if (city === null) {break;}{
+       document.write(`<li>${cities[index] + " "}</li><br>`);
+    }
+}
+// for (let index = 0; index < city.length; index++);
+
+document.write("</ul>");
+
+
