953a508728c272fd62f13e2c6c22281eec3cc17d
Azul De Pol <azul.depol@gmail.com>
2023-03-20 22:51:41 +0000

    empeze a trabajar el ejercicio 6

--- js/TP3-ejercicio3.js
+++ js/TP3-ejercicio3.js
@@ -4,3 +4,4 @@
 Ejemplo de salida:
 
 */ 
+
--- js/TP3-ejercicio6.js
+++ js/TP3-ejercicio6.js
@@ -16,3 +16,10 @@ lado B = 5
 Output: 58
 
 */ 
+
+let largo = parseInt(prompt('Ingrese el valor de un lado de un rectángulo'));
+let ancho = parseInt(prompt('Ingrese el valor de un lado de un rectángulo'));
+const perimetro = function area (largo, ancho) {
+    return largo * ancho
+    }
+document.write(`El perimetro del rectángulo es ${perimetro}`)
